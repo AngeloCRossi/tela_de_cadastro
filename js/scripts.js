9ebbc5cb0ebb8cc65dea48e426f69a4edf3a422c
Angelo Rossi <yrc_legna@hotmail.com>
2023-02-12 17:27:22 -0300

    ainda acertando campos da aba visualização

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $("input#nu_cpfcnpj").inputmask({mask: ['999.999.999-99'], keepStatic: true});
     $("input#vl_movimentacao").inputmask({mask: ['R$ 999.999,00'], keepStatic: false});
-    dt_movimentacao
 });
 
 
@@ -12,7 +11,7 @@ $(document).on("change","select#tp_pessoa", function(){
     var targetLabel = $("#lbl_nu_cpfcpnj");
     var selectedOptionLabel = $(this).children("option:selected")[0].label;
 
-    console.log(this);
+    //console.log(this);
 
     //Agora vou alterar o label do campo CPF/CNPJ e habilitar ou desabilitar o input
     switch(selectedOptionLabel){
@@ -37,14 +36,31 @@ $(document).on("change","select#tp_pessoa", function(){
 
 })
 
-
-document.getElementById("btn_cadastrar").addEventListener("click",function() {
-    var valor_CPF = document.getElementById("input#nu_cpfcnpj").value;
-    var valor_nome = document.getElementById("input#no_cliente").value;
-    var valor_produto = document.getElementById("select#no_produto").name;
-    document.getElementById("nu_cpfcnpj_visualização").value = valor_CPF;
-
+//Evitando que a página seja atualizada quando o botão cadastrar for clicado
+const form = document.getElementById("form_cadastro");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
 
 
 
-})
+window.onload = function() {
+    document.getElementById("btn_cadastrar").addEventListener("click", function(event) {
+        
+        //console.log(moment("dt_movimentacao").format('L'));
+        
+        //setando variaveis para pegar os dados
+        var vlr_CPF = document.getElementById("nu_cpfcnpj").value;
+        var vlr_nome = document.getElementById("no_cliente").value;
+        var vlr_produto = document.getElementById("no_produto").text;
+        var vlr_dt_movimentacao = document.getElementById("dt_movimentacao").value;
+        var vlr_vl_movimentacao = document.getElementById("vl_movimentacao").value;
+
+        //preenchendo o campo destino
+        document.getElementById("nu_cpfcnpj_visualização").value = vlr_CPF;
+        document.getElementById("no_cliente_visualizacao").value = vlr_nome;
+        document.getElementById("no_produto_visualização").value = vlr_produto  ;
+        document.getElementById("dt_movimentacao_visualização").value = vlr_dt_movimentacao;
+        document.getElementById("vl_movimentacao_visualização").value = vlr_vl_movimentacao;
+    })
+};
